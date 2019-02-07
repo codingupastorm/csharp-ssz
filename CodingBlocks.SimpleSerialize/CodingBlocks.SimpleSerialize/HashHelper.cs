7f4123710cd20cc24b13003b1c9da89f573594ed
Jordan Andrews <jordandrews@hotmail.com>
2019-02-07 16:57:32 +1100

    Skeleton

---
+++ CodingBlocks.SimpleSerialize/CodingBlocks.SimpleSerialize/HashHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HashLib;
+
+namespace CodingBlocks.SimpleSerialize
+{
+    public static class HashHelper
+    {
+        public static byte[] Keccak256(byte[] input)
+        {
+            return HashFactory.Crypto.SHA3.CreateKeccak256().ComputeBytes(input).GetBytes();
+        }
+    }
+}
