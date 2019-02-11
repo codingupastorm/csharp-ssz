b99bb73e8a554778d236f006c4508a91a293fecf
Jordan Andrews <jordandrews@hotmail.com>
2019-02-11 21:19:47 +1100

    Region for encoding

--- CodingBlocks.SimpleSerialize/CodingBlocks.SimpleSerialize/SSZ.cs
+++ CodingBlocks.SimpleSerialize/CodingBlocks.SimpleSerialize/SSZ.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CodingBlocks.SimpleSerialize
 {
     public static class SSZ
     {
-        /*
-         * There are three 'primitive' data types:
-         * - uintN
-         * - bytes
-         * - bool
-         */
-
+        #region Encoding
+        /// <summary>
+        /// Encode the 'uint' primitive type.
+        /// </summary>
+        /// <param name="value">The value to encode.</param>
+        /// <param name="bits">The number of bits that make up the integer. Must be a byte number (multiple of 8).</param>
+        /// <returns>Encoded uint.</returns>
         public static byte[] EncodeUint(ulong value, int bits)
         {
             CheckIntBitSize(bits);
@@ -31,6 +32,57 @@ namespace CodingBlocks.SimpleSerialize
 
             return encoded;
         }
+
+        public static byte[] EncodeBool(bool value)
+        {
+            return value
+                ? new byte[] {0x01}
+                : new byte[] {0x00};
+        }
+
+        public static byte[] EncodeBytes(byte[] bytes)
+        {
+            // TODO: Check length of value
+            // TODO: Performance? 
+            byte[] lengthBytes = EncodeUint((ulong) bytes.Length, 32);
+            return lengthBytes.Concat(bytes).ToArray();
+        }
+
+        public static byte[] EncodeBytesList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static byte[] EncodeBoolList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static byte[] EncodeUintList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static byte[] EncodeContainer(IList<byte[]> fieldValues)
+        {
+            // TODO: Check on total length of values.
+            // TODO: Optimize performance.
+            int totalLength = fieldValues.Sum(x => x.Length);
+            byte[] serializedContainer = new byte[totalLength];
+            int cursor = 0;
+
+            foreach (var field in fieldValues)
+            {
+                Array.Copy(field, 0, serializedContainer, cursor, field.Length);
+                cursor += field.Length; 
+            }
+
+            byte[] lengthBytes = EncodeUint((ulong) totalLength, 32);
+
+            return lengthBytes.Concat(serializedContainer).ToArray();
+        }
+
+        #endregion
 
         /// <summary>
         /// Check number of bits always a whole byte number.
